b311bc69af2417d18c96fcf8a31e51ffe246258b
Vinaysrb <vinaysr@yahoo.com>
2022-07-29 14:25:56 +0100

    Add launch.json and tidy up app_call file

--- todo_app/trello_app_call.py
+++ todo_app/trello_app_call.py
@@ -1,19 +1,17 @@
-from turtle import dot
 import dotenv
 import requests
-import dotenv
 import os
 
 url = "https://api.trello.com/1/members/me/boards"
 
 env_file = dotenv.load_dotenv(".env")
-print(os.getenv("Trello_API_KEY"))
-querystring = {
-"key":os.getenv("Trello_API_KEY"),
-"token":os.getenv("Trello_API_TOKEN")
+print(os.getenv("TRELLO_API_KEY"))
 
+querystring = {
+    "key": os.getenv("TRELLO_API_KEY"),
+    "token": os.getenv("TRELLO_API_TOKEN")
 }
 
-response = requests.request("GET", url, params=querystring)
+response = requests.get(url, params=querystring)
 
 print(response.text)
